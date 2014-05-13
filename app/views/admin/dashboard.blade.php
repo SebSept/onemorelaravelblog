dec3228ef8850d261738ee9bf22126e5910b608c
SebSept <sebastienmonterisi@yahoo.fr>
2014-05-13 13:47:56 +0200

    fix links in dashboard

--- app/views/admin/dashboard.blade.php
+++ app/views/admin/dashboard.blade.php
@@ -25,9 +25,20 @@
 			<td>{{{ $post->published }}}</td>
 			<td>{{{ $post->created_at }}}</td>
 			<td>{{{ $post->updated_at }}}</td>
-			<td>{{ link_to_route('admin.edit', 'Edit', ['id' => $post->id]) }}</td>
-			<td>{{ link_to_route('admin.togglePublished', $post->published ? 'unpublish' : 'publish', ['id' => $post->id]) }}</td>
-			<td>{{ link_to_route('admin.delete', 'Delete', ['id' => $post->id]) }}</td>
+			<td><a href="{{ route('admin.edit', ['id' => $post->id]) }}" title="Modifier"><span class="glyphicon glyphicon-search"></span></a></td>
+			
+			<!-- <td>{{ link_to_route('admin.edit', 'Edit', ['id' => $post->id]) }}</td> -->
+			<td><a href="{{ route('admin.togglePublished', ['id' => $post->id]) }}" title="{{ $post->published ? 'Dépublier' : 'Publier' }}">
+				@if($post->published) 
+					<span class="glyphicon glyphicon-remove-sign"></span>
+				@else
+					<span class="glyphicon glyphicon-ok-sign"></span>
+				@endif	
+			</a></td>
+			
+
+			<td><a href="{{ route('admin.delete', ['id' => $post->id]) }}" title="Supprimer"><span class="glyphicon glyphicon-trash"></span></a></td>
+			
 		</tr>
 	@endforeach
 	</tbody>
