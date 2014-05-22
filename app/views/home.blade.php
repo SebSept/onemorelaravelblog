71ef3caedfa488e0093ae2196df2ad9edf59bc50
SebSept <sebastienmonterisi@yahoo.fr>
2014-05-22 23:33:10 +0200

    improved views

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,16 +1,17 @@
 @extends('layout')
 
 @section('content')
+<div class="container" id="list">
+    @if(isset($list_title) && $list_title)
+        <h1>{{ $list_title }}</h1>
+    @endif
 
-	@if(isset($list_title) && $list_title)
-		<h1>{{ $list_title }}</h1>
-	@endif
+    @foreach($posts AS $post)
+        <h2>{{ link_to_route('post.view', $post->title, ['slug' => $post->slug]) }}</h2>
+        <p>{{{ $post->teaser }}}</p>
+        <p>{{ link_to_route('post.view', $post->title, ['slug' => $post->slug]) }}</p>
+    @endforeach
 
-	@foreach($posts AS $post)
-		<h2>{{ link_to_route('post.view', $post->title, ['slug' => $post->slug]) }}</h2>
-		<p>{{{ $post->teaser }}}</p>
-		<p>{{ link_to_route('post.view', $post->title, ['slug' => $post->slug]) }}</p>
-	@endforeach
-
-	<?php echo $posts->links(); ?>
+    <?php echo $posts->links(); ?>
+</div>
 @stop
--- app/views/post.blade.php
+++ app/views/post.blade.php
@@ -1,33 +1,41 @@
 @extends('layout')
 
 @section('content')
-	<h1>{{{ $post->title }}}</h1>
 
-	<p><?php echo Markdown::render($post->content); ?></p>
+    <div class="container" id="post-content">
+        <h1>{{{ $post->title }}}</h1>
 
-	@if(count($post->tags))
-		<h2>{{ trans('front.tags.header') }}</h2>
+        <p><?php echo Markdown::render($post->content); ?></p>
+    </div>
 
-		<ul class="list-inline">
-		@foreach($post->tags AS $tag)
-			<li>@tag($tag)</li>
-		@endforeach
-		</ul>
-	@endif
+    @if(count($post->tags))
+        <div class="container" id="post-tags">
+            <h2>{{ trans('front.tags.header') }}</h2>
 
-	@if(count($post->comments))
-		<h2>{{ trans('front.comments.header') }}</h2>
-		
-		<ul>
-		@foreach($post->comments AS $comment)
-			<li>
-				<h3>{{ $comment->title }}</h3>
-				<p>{{ $comment->content }}</p>
-			</li>
-		@endforeach
-		</ul>
-	@endif
+            <ul class="list-inline">
+            @foreach($post->tags AS $tag)
+                    <li>@tag($tag)</li>
+            @endforeach
+            </ul>
+        </div>
+    @endif
+    
+    @if(count($post->comments))
+    <div class="container" id="post-comments">
+        <h2>{{ trans('front.comments.header') }}</h2>
 
+        <ul>
+        @foreach($post->comments AS $comment)
+                <li>
+                        <h3>{{ $comment->title }}</h3>
+                        <p>{{ $comment->content }}</p>
+                </li>
+        @endforeach
+        </ul>
+    </div>
+    @endif
+
+    <div class="container" id="post-form">
 	<h3>{{ trans('front.comment.header') }}</h3>
 	{{ Form::open(['route' => ['comment.add', 'post_id' => $post->id], 'class' => 'form-horizontal', 'role' => 'form'  ]) }}
 
@@ -38,4 +46,5 @@
 
 		{{ Form::submit(trans('front.comment.submit'), ['class' => 'col-md-offset-2']) }}
 	{{ Form::close() }}
+    </div>
 @stop
