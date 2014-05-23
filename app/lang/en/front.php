95bd5d45f853213d43e2bca1eefddf280e899c9f
SebSept <sebastienmonterisi@yahoo.fr>
2014-05-23 22:29:43 +0200

    en/us translations

--- app/lang/en/front.php
+++ app/lang/en/front.php
@@ -1,6 +1,19 @@
 <?php
 
 return [
-    'Tags' => 'Tags',
-    'Your comment' => 'Your comment'
+   'tags.header' => 'Tags',
+    
+    // post view comment form
+    'comments.header' => 'Comments',
+    'comment.header' => 'Comment ? ',
+    'comment.title' => 'Title',
+    'comment.name' => 'Name',
+    'comment.url' => 'website',
+    'comment.content' => 'Comment',
+    'comment.submit' =>  'Send',
+    
+    // lists of posts filtered by tag
+    // home.blade.php + route.php
+    'list.header.posts tagged' => 'Posts tagged <em>:title</em>',
+    
 ];
