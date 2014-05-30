d2c7c28abef6a7237a42aad639b5cc3c5dd3aa14
SebSept <sebastienmonterisi@yahoo.fr>
2014-05-30 11:37:33 +0200

    config local via apache

--- app/config/local/app.php
+++ app/config/local/app.php
@@ -2,7 +2,8 @@
 
 return array(
 	'debug' => true,
-	'url' => 'http://localhost:8000',
+//	'url' => 'http://localhost:8000',
+        'url' => 'http://localhost/perso/lblog/public',
 	'timezone' => 'UTC',
 	'locale' => 'fr',
 	'fallback_locale' => 'en',
