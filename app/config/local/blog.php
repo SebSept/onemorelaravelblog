a6be58725b5268ea924db407db0ffedf4c7072fe
SebSept <sebastienmonterisi@yahoo.fr>
2014-06-03 23:01:53 +0200

    Revert "locale config"
    This reverts commit 3fd557b03c5e260a4bf15d0b0b7a2bc07f655c88.

--- app/config/local/blog.php
+++ app/config/local/blog.php
@@ -5,13 +5,5 @@
  */
 return [
     'title' => 'Seb7 DevBlog',
-    'subtitle' => 'Billets à propos mes aventures de developpeur web.',
-
-	// Posts per page on home
-	'posts_per_page' => 5,
-	'comments_per_page_admin' => 25,
-	'posts_per_page_admin' => 25,
-	// locale for dates	
-	'locale' => 'fr_FR',
-    'dateformat' => '%d %B %Y',
+    'subtitle' => 'Billets à propos mes aventures de developpeur web.'
 ];
