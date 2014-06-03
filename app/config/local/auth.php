50f18ebb676467750868a87413fdadbe5847363f
SebSept <sebastienmonterisi@yahoo.fr>
2014-06-03 23:01:54 +0200

    Revert "separation dev/perso"
    This reverts commit fa56b483f801e69617e1b76de8fb9f9964644bad.

--- app/config/local/auth.php
+++
@@ -1,6 +0,0 @@
-<?php
-// a versionner dans branche monblog
-return array(
-    'user' => 'seb',
-    'password' => '$2y$10$IGbzHP8rMH/V5KyKxuZVCOOrBizK/z/NJJP4VlTe4lSoiLHKyn2OC'
-);
--- app/config/local/blog.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Blog configuration
- * 
- */
-return [
-    'title' => 'Seb7 DevBlog',
-    'subtitle' => 'Billets à propos mes aventures de developper web.'
-];
