c006182ec2b15af9a9cd93b4ce97053fc4dd69bc
SebSept <sebastienmonterisi@yahoo.fr>
2014-06-03 23:01:53 +0200

    Revert "config mon de passe"
    This reverts commit 94b4461df8b21dd5dfc87375d5022cae76d20f96.

--- app/config/local/blog.php
+++ app/config/local/blog.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Blog configuration
  * 
@@ -7,14 +6,12 @@
 return [
     'title' => 'Seb7 DevBlog',
     'subtitle' => 'Billets à propos mes aventures de developpeur web.',
-    // Posts per page on home
-    'posts_per_page' => 5,
-    'comments_per_page_admin' => 25,
-    'posts_per_page_admin' => 25,
-    // locale for dates	
-    'locale' => 'fr_FR',
+
+	// Posts per page on home
+	'posts_per_page' => 5,
+	'comments_per_page_admin' => 25,
+	'posts_per_page_admin' => 25,
+	// locale for dates	
+	'locale' => 'fr_FR',
     'dateformat' => '%d %B %Y',
-    // admin credentials
-    'password' => '$2y$10$BfiQeJ0wjNkCkFlK93t8s.iVGNVBapUxcmd8n/eQBrONEuHwjVxcy',
-    'user' => 'seb',
 ];
