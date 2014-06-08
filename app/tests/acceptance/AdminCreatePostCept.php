22af3484f245cd1ed3b5c368cda4bbe3ccc91b7e
SebSept <sebastienmonterisi@yahoo.fr>
2014-06-08 03:18:13 +0200

    added tags to test 'admin create post' (fails)
    tests fails because of bug #10

--- app/tests/acceptance/AdminCreatePostCept.php
+++ app/tests/acceptance/AdminCreatePostCept.php
@@ -10,10 +10,11 @@ $I->submitForm('form', [
     'title' => 'My new post',
     'slug' => 'my-new-post',
     'teaser' => 'The post summary.',
-    'content', '#This is content',
+    'content' => '#This is content',
+    'hidden-tags' =>  Tag::all()->get(0)->title.','.
+                    Tag::all()->get(1)->title.','.
+                    Tag::all()->get(3)->title.','. 
+                    'newone'
 ]);
 
 $I->see(trans('admin.post.saved'));
-
-
-
