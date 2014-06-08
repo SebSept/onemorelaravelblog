57982a7420c27e9f41be7d9dd6cfb8fdf1fefe21
SebSept <sebastienmonterisi@yahoo.fr>
2014-06-08 02:47:54 +0200

    added 'hidden-tags' in admi.post.edit view

--- app/views/testing/admin/edit.blade.php
+++ app/views/testing/admin/edit.blade.php
@@ -5,14 +5,17 @@
 	<h1>{{{ $post->title }}}</h1>
 
 	{{ Form::model($post, ['route'=> [ 'admin.post.submit', 'id'=> $post->id ], 'class' => 'form-horizontal', 'role' => 'form' ]) }}
-
+        
 		{{ Form::myInput('title', 'text', 'Titre') }}
 		{{ Form::myInput('slug', 'text', 'URL') }}
 		{{ Form::myInput('teaser', 'textarea', 'Résumé') }}
 		{{ Form::myInput('content', 'textarea', 'Contenu') }}
 		{{ Form::myInput('published', 'checkbox', 'Publié') }}
 
-		{{ Form::tagsSelector('tags', 'Tags') }}
+		{{-- Form::tagsSelector('tags', 'Tags') --}}
+                
+                {{-- just add 'hidden-tags' field that js generate in tag selector --}}
+                {{ Form::text('hidden-tags') }}
 		
 		{{ Form::submit() }}
 
