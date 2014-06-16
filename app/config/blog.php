78ef4a761a626abf38bc08d4340b2f9dcf8a39de
SebSept <sebastienmonterisi@yahoo.fr>
2014-06-16 21:59:18 +0200

    fix previous commit

--- app/config/blog.php
+++ app/config/blog.php
@@ -20,8 +20,8 @@ return [
     'locale' => 'en_US',
     'dateformat' => '%B %d, %Y',
     // admin credentials
-    'password' => Hash::make('test'),
-    'user' => 'seb',
+    'password' => '',
+    'user' => '',
     // theme
     // changing theme name implies creating 
     // - /app/view/theme-name dir
