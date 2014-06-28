812775a9bcbb597a8ec52f609e59282c63382343
SebSept <sebastienmonterisi@yahoo.fr>
2014-06-28 23:41:15 +0200

    [test] post comment as admin - #2

--- app/tests/acceptance/PostSubmitCommentValidAsAdminCept.php
+++ app/tests/acceptance/PostSubmitCommentValidAsAdminCept.php
@@ -3,6 +3,10 @@
  * @todo enable filter / check csrf
  * @todo check urls contains "#comment_submitted"
  */
+
+$faker = Faker\Factory::create('fr_FR');
+$submited_content = $faker->text;
+
 DB::beginTransaction();
 
 // we do not test csrf filter, we can rely on laravel
@@ -10,28 +14,21 @@ DB::beginTransaction();
 Route::disableFilters();
 
 $I = new WebGuy($scenario);
-$I->amGuest();
-$I->wantTo('submit valid comment as guest');
-$I->amOnPage('aut-ex-accusantium-quo-tenetur-harum');
+$I->wantTo('submit valid comment as admin');
+$I->amAdmin();
+//$I->amHttpAuthenticated('testguy', 'pass');
 
-echo  $I->grabValueFrom('input[name=_token]');
-echo  $I->grabValueFrom('input[name=title]');
+// step 1 : submit comment
+$I->amOnPage('aut-ex-accusantium-quo-tenetur-harum');
 
 $I->submitForm( '#post-form form', [  'title'=> 'my comment title' ,
                                 'author_name' => 'Jacky kio',
                                 'author_site' => 'http://google.fr',
-                                'content' => 'bla bla. Fake contetnt',
+                                'content' => $submited_content,
 //                                '_token' => $I->grabValueFrom('input[name="_token"]')
         ]);
-//$I->canSeeCurrentUrlEquals($uri)
-//if($I->seeCurrentUrlEquals('/aut-ex-accusantium-quo-tenetur-harum?#comment_submitted')) {
-//    echo 'oki';
-//}
-//else
-//    echo 'notOki';
 
-//echo $I->grabFromCurrentUrl('~/aut-ex-accusantium-quo-tenetur-harum(.+)~');
-//$I->
-//$I->seeCurrentUrlEquals('/aut-ex-accusantium-quo-tenetur-harum?#comment_submitted');
+// step 2 : check comment is published
+$I->see($submited_content);
 
 DB::rollBack();
