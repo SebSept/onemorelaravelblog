919b031d9a4e779652d35dba5ebe97cd6244eb6e
SebSept <sebastienmonterisi@yahoo.fr>
2014-06-30 11:55:13 +0200

    [test] fix db rollback

--- app/tests/acceptance/AdminModerate_NoCommentsToModerateCept.php
+++ app/tests/acceptance/AdminModerate_NoCommentsToModerateCept.php
@@ -3,15 +3,15 @@ use \SebSept\OMLB\Models\Comment\Comment;
 
 Route::enableFilters();
 $I = new WebGuy($scenario);
-DB::transaction( function()  use ($I)
-{
-    DB::delete('DELETE FROM '.(new Comment)->getTable());
-    $I->wantTo('Be on comment list (admin) - without comments');
 
-    $I->amHttpAuthenticated('testguy', 'pass');
-    $I->amOnPage('/admin/comment/moderate');
+DB::beginTransaction();
 
-    $I->see(trans('admin.comment.moderate'));
-    DB::rollBack();
-});
+DB::delete('DELETE FROM '.(new Comment)->getTable());
+$I->wantTo('Be on comment list (admin) - without comments');
 
+$I->amHttpAuthenticated('testguy', 'pass');
+$I->amOnPage('/admin/comment/moderate');
+
+$I->see(trans('admin.comment.moderate'));
+
+DB::rollBack();
