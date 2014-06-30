f2f7cc69cacf9a70d5b26cfd9c8d062c241784c5
SebSept <sebastienmonterisi@yahoo.fr>
2014-06-30 11:33:48 +0200

    removed unused var (+format)

--- app/database/seeds/TagsPostsPivotTableSeeder.php
+++ app/database/seeds/TagsPostsPivotTableSeeder.php
@@ -1,29 +1,36 @@
 <?php
+/**
+ * TagsPostsPivotTableSeeder
+ *
+ * seed pivot table posts-tags : tags associated with posts
+ * 
+ * @author sebastienmonterisi@yahoo.fr
+ * @package onemorelaravelblog
+ */
+class TagsPostsPivotTableSeeder extends Seeder
+{
 
-class TagsPostsPivotTableSeeder extends Seeder {
+    public function run()
+    {
+        $associations = [];
 
-	public function run()
-	{
-		$faker = Faker\Factory::create('fr_FR');
-		$associations = [];
+        $post_id = 0;
+        while ($post_id++ < 20) {
+            $tag_id = 0;
+            $tag_id_array = [];
+            while ($tag_id++ < 7) {
+                $tag_id_array[] = rand(1, 7);
+            }
+            $tag_id_array = array_unique($tag_id_array);
 
-		$post_id = 0;
-		while($post_id++ < 20) {
-			$tag_id = 0;
-			$tag_id_array = [];
-			while($tag_id++ < 7)
-			{
-				$tag_id_array[] = rand(1,7);
-			}
-			$tag_id_array = array_unique($tag_id_array);
-			
-			foreach($tag_id_array AS $tag_id) {
-				$associations[] = ['post_id' => $post_id, 'tag_id' => $tag_id];
-			}
-		}
+            foreach ($tag_id_array AS $tag_id)
+            {
+                $associations[] = ['post_id' => $post_id, 'tag_id' => $tag_id];
+            }
+        }
 
-		DB::table('post_tag')->truncate();
+        DB::table('post_tag')->truncate();
         DB::table('post_tag')->insert($associations);
-	}
+    }
 
 }
