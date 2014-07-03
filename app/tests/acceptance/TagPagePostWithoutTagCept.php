2c5ae2976df796f1609d42d1baf5ffd32832cbea
SebSept <sebastienmonterisi@yahoo.fr>
2014-07-03 11:39:04 +0200

    increase blog.posts_per_page config val to ensure that the item is not pub (post filtered by tag)

--- app/tests/acceptance/TagPagePostWithoutTagCept.php
+++ app/tests/acceptance/TagPagePostWithoutTagCept.php
@@ -1,5 +1,9 @@
 <?php
 $I = new WebGuy($scenario);
 $I->wantTo('Post with specified tag does not appears on tag page');
+
+// just to be sure that the item is not published but visible on another page.
+Config::set('blog.posts_per_page', 2000);
+
 $I->amOnPage('tag/accusamus'); // tag->id = 2
 $I->dontSee('Qui amet ipsum sunt.');
