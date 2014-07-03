47e5736f9459419557bb50f04a4c2fc300ed18f1
SebSept <sebastienmonterisi@yahoo.fr>
2014-07-03 09:34:56 +0200

    increase blog.posts_per_page config val to ensure that the item is not published but visible on another page.

--- app/tests/acceptance/HomePageNotPublishedPostCept.php
+++ app/tests/acceptance/HomePageNotPublishedPostCept.php
@@ -1,6 +1,10 @@
 <?php
 $I = new WebGuy($scenario);
 $I->wantTo('Not published post does not appears on home');
+
+// just to be sure that the item is not published but visible on another page.
+Config::set('blog.posts_per_page', 2000);
+
 $I->amOnPage('/');
 $I->dontSee('Quo eos natus unde cum occaecati.');
 
